03c65d771f965439e665a891b9f2cacda3fdb901
unknown <ramsamy.wjp@gmail.com>
2025-02-18 18:15:04 +0400

    Mise à jour adresse url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://rsm-ecommerce.vercel.app',
   credentials: true,
 }))
 
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -30,8 +30,8 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: 'http://localhost:5173/cancel'
+      success_url: 'https://rsm-ecommerce.vercel.app/success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://rsm-ecommerce.vercel.app/cancel'
     });
 
     res.json({ id: session.id });
